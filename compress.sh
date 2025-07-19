8f09a0c62a5d2e53e94e48d6b6fcc8c5629aef21
Ivan Smitka <ivan@smitka.net>
2025-07-19 17:05:22 +0200

    Version Management

--- compress.sh
+++ compress.sh
@@ -1,4 +1,9 @@
-(cd ../ && zip -r wp-event-sticker/wp-event-sticker.zip \
+#!/bin/bash
+sass src/event-sticker.scss static/event-sticker.css
+VERSION=$(git tag -l)
+echo "${VERSION}"
+sed -i -E "s/^ \* Version\: .*$/ * Version: ${VERSION}/g" wp-event-sticker.php
+(cd ../ && zip -r wp-event-sticker/${VERSION}.zip \
   wp-event-sticker/index.html \
   wp-event-sticker/LICENSE \
   wp-event-sticker/README.md \
